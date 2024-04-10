912ce85292dc5ea7542d103b6412c899db9548af
Jumpersky77 <zkalineuszo008@gmail.com>
2024-04-10 10:22:28 +0200

    dodanie strony wyswietlania przedmiotow z lokalnej tablicy

--- AppPraktyki-main/src/App.tsx
+++ AppPraktyki-main/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Subjects from "./pages/Subjects";
 function App() {
   return (
   <>
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Login />} /> |
 
       <Route path="/register" element={<Register />} />
+
+      <Route path="/subjects" element={<Subjects />} />
     </Routes>
 
   </>
