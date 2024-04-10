8fd98eda0cbec36bc911ae7804b215b781d677f6
Jumpersky77 <zkalineuszo008@gmail.com>
2024-04-10 14:02:57 +0200

    subject onclick

--- AppPraktyki-main/src/pages/Subjects.tsx
+++ AppPraktyki-main/src/pages/Subjects.tsx
@@ -5,22 +5,26 @@ import { Link } from "react-router-dom";
 import Header from "../components/Header";
 
 const _subjects = ["Matematyka", "Chemia", "Biologia"];
-type Props = { text: string }
+type Props = { text: string, onSubjectClick: (subject: string) => void }
 function Subject(props : Props) {
     return (
         <div>
-            <Button variant="outlined">{props.text}</Button>
+            <Button variant="outlined" onClick={() => props.onSubjectClick(props.text)}>{props.text}</Button>
         </div>
     )
 }
 
 const Subjects = () => {
+    const SubjectClick = (text : string) => {
+        console.log(text)
+        //TO DO redirect
+    };
     return (
         <>
             <Header accountName={"example"} subscriptionDaysLeft={"14"}></Header>
             <br/>
             {_subjects.map((subject, index) => (
-                <Subject key={index} text={subject}></Subject>
+                <Subject key={index} text={subject} onSubjectClick={SubjectClick}></Subject>
             ))}
         </>
     )
