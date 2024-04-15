a8118b86f9b39c6156f18f5de2b5590befecf4f2
Jumpersky77 <zkalineuszo008@gmail.com>
2024-04-15 10:44:07 +0200

    details update

--- AppPraktyki-main/src/pages/Details.tsx
+++ AppPraktyki-main/src/pages/Details.tsx
@@ -9,27 +9,32 @@ const _details = [{
     name: "Równania kwadratowe",
     author: "Miłosz",
     rating: "7.6",
+    text: "Zad1: a, zad2: b",
 },
 {
     id: "1",
     name: "Planimetria",
     author: "Bartosz",
     rating: "4.9",
+    text: "Zad1: b, zad2: a",
 }]
 
 const _comments=[
     {
         id_User:0,
+        user_name: "MiloszTT",
         Text: "Dobra robota  d=====(￣▽￣*)b",
         id_details:0
     },
     {
         id_User:1,
+        user_name: "BartłomiejP",
         Text: "odp a w z. 1 w gr A jest zle",
         id_details:0
     },
     {
         id_User:2,
+        user_name: "Skowronek_andrzej",
         Text: "jakis kreatywny komentarz",
         id_details:1
     }
@@ -39,8 +44,9 @@ const commentblock=(id:number, aid:number)=>{
     if(_comments[id].id_details==aid){
             return  (
                 <div >
-                    <div>{_comments[id].id_User}</div>
+                    <div>{_comments[id].user_name}</div>
                     <div>{_comments[id].Text}</div>
+                    <br />
                 </div>
             )
         }
@@ -58,7 +64,7 @@ const Details = () => {
                 <h2>{_details[answerID].name}</h2>
             </div>
             <div>
-
+                {_details[answerID].text}
             </div>
             <h4>{_details[answerID].rating}</h4>
             <div>
