290c8b62935b3db1db5a32335ce6960d2f636485
Marek Mialkowski <wolowina.powodowac.0w@icloud.com>
2024-04-21 18:11:12 +0200

    adjusted router structure

--- AppPraktyki-main/src/pages/AddAnswer.tsx
+++ AppPraktyki-main/src/pages/AddAnswer.tsx
@@ -1,77 +1,77 @@
 import Header from "../components/Header";
-import React from 'react';
+import React from "react";
 import { UserName } from "../App";
 import { useContext, useState } from "react";
 import {
-    Avatar,
-    Box,
-    Button,
-    Container,
-    CssBaseline,
-    Grid,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  import { Link } from "react-router-dom";
-  import { useLocation } from 'react-router-dom';
-  import { useNavigate } from 'react-router-dom';
-  import { styled } from '@mui/material/styles';
-  import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-  import Radio from '@mui/material/Radio';
-  import RadioGroup from '@mui/material/RadioGroup';
-  import FormControlLabel from '@mui/material/FormControlLabel';
-  import FormControl from '@mui/material/FormControl';
-  import FormLabel from '@mui/material/FormLabel';
-const AddAnswer=()=>{
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import { useAppNavigation } from "../router/router";
+const AddAnswer = () => {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
 
-    const [title, setTitle] = useState("");
-    const [text, setText]= useState("");
-    
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const subject = searchParams.get('subject');
-    const teacher = searchParams.get('teacher');
-    
-    
-    
-    const VisuallyHiddenInput = styled('input')({
-        clip: 'rect(0 0 0 0)',
-        clipPath: 'inset(50%)',
-        height: 1,
-        overflow: 'hidden',
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        whiteSpace: 'nowrap',
-        width: 1,
-      });
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const subject = searchParams.get("subject");
+  const teacher = searchParams.get("teacher");
 
+  const navigation = useAppNavigation();
 
-      const [previewImages, setPreviewImages] = useState<string[]>([]);
+  const VisuallyHiddenInput = styled("input")({
+    clip: "rect(0 0 0 0)",
+    clipPath: "inset(50%)",
+    height: 1,
+    overflow: "hidden",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    whiteSpace: "nowrap",
+    width: 1,
+  });
 
-      const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const files = event.target.files;
-        if (files) {
-          const imageFiles = Array.from(files).filter(file => file.type.startsWith('image'));
-          const imageUrls = imageFiles.map(file => URL.createObjectURL(file));
-          setPreviewImages(prevImages => [...prevImages, ...imageUrls]);
-        }
-      }
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
 
-       const [type, setType] = useState('');
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const imageFiles = Array.from(files).filter((file) =>
+        file.type.startsWith("image")
+      );
+      const imageUrls = imageFiles.map((file) => URL.createObjectURL(file));
+      setPreviewImages((prevImages) => [...prevImages, ...imageUrls]);
+    }
+  };
 
-      const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setType((event.target as HTMLInputElement).value);
-      };
+  const [type, setType] = useState("");
 
-      //console.log(title, type, previewImages)
+  const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setType((event.target as HTMLInputElement).value);
+  };
 
-    const handleAddAnswer = async () => {};
+  //console.log(title, type, previewImages)
 
-    return(
-        <>
-        <Header accountName={useContext(UserName)} subscriptionDaysLeft={"14"} />
-        <div>
+  const handleAddAnswer = async () => {};
+
+  return (
+    <>
+      <div>
         <Container maxWidth="xs">
           <CssBaseline />
           <Box
@@ -98,48 +98,75 @@ const AddAnswer=()=>{
                     onChange={(e) => setTitle(e.target.value)}
                   />
                 </Grid>
-                
+
                 <Grid item xs={12}>
                   <TextField
                     id="outlined-multiline-flexible"
                     label="Tu możesz wpisać odpowiedzi"
                     multiline
                     fullWidth
-                    onChange={(e)=>setText(e.target.value)}
+                    onChange={(e) => setText(e.target.value)}
                   />
-
                 </Grid>
 
                 <Grid item xs={12}>
-                    <Button
-                        fullWidth
-                        component="label"
-                        role={undefined}
-                        variant="contained"
-                        tabIndex={-1}
-                        startIcon={<CloudUploadIcon />}
-                        >
-                        Dodaj zdjęcia
-                        <VisuallyHiddenInput type="file" accept="image/*"  multiple onChange={handleFileChange}/>
-                    </Button> 
-                    {previewImages.map((imageUrl, index) => (
-                    <img key={index} src={imageUrl} alt={`Preview ${index}`} style={{ maxWidth: '100%', marginTop: '10px', display: 'block' }} />
-                    ))}
+                  <Button
+                    fullWidth
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<CloudUploadIcon />}
+                  >
+                    Dodaj zdjęcia
+                    <VisuallyHiddenInput
+                      type="file"
+                      accept="image/*"
+                      multiple
+                      onChange={handleFileChange}
+                    />
+                  </Button>
+                  {previewImages.map((imageUrl, index) => (
+                    <img
+                      key={index}
+                      src={imageUrl}
+                      alt={`Preview ${index}`}
+                      style={{
+                        maxWidth: "100%",
+                        marginTop: "10px",
+                        display: "block",
+                      }}
+                    />
+                  ))}
                 </Grid>
                 <Grid item xs={12}>
-                    <FormControl>
-                        <FormLabel id="demo-row-radio-buttons-group-label">Typ odpowiedzi</FormLabel>
-                        <RadioGroup
-                            row
-                            aria-labelledby="demo-row-radio-buttons-group-label"
-                            name="row-radio-buttons-group"
-                            onChange={handleRadioChange}
-                        >
-                            <FormControlLabel value="sprawdzian" control={<Radio />} label="Sprawdzian" />
-                            <FormControlLabel value="kartkówka" control={<Radio />} label="Kartkówka" />
-                            <FormControlLabel value="inne" control={<Radio />} label="Inne" />
-                        </RadioGroup>
-                    </FormControl>
+                  <FormControl>
+                    <FormLabel id="demo-row-radio-buttons-group-label">
+                      Typ odpowiedzi
+                    </FormLabel>
+                    <RadioGroup
+                      row
+                      aria-labelledby="demo-row-radio-buttons-group-label"
+                      name="row-radio-buttons-group"
+                      onChange={handleRadioChange}
+                    >
+                      <FormControlLabel
+                        value="sprawdzian"
+                        control={<Radio />}
+                        label="Sprawdzian"
+                      />
+                      <FormControlLabel
+                        value="kartkówka"
+                        control={<Radio />}
+                        label="Kartkówka"
+                      />
+                      <FormControlLabel
+                        value="inne"
+                        control={<Radio />}
+                        label="Inne"
+                      />
+                    </RadioGroup>
+                  </FormControl>
                 </Grid>
               </Grid>
               <Button
@@ -152,15 +179,21 @@ const AddAnswer=()=>{
               </Button>
               <Grid container justifyContent="flex-end">
                 <Grid item>
-                  <Link to={`/answers?subject=${subject}&teacher=${teacher}`}>Wróć do listy odpowiedzi</Link>
+                  <Link
+                    to={navigation.getAnswerPageLink(
+                      String(subject),
+                      String(teacher)
+                    )}
+                  >
+                    Wróć do listy odpowiedzi
+                  </Link>
                 </Grid>
               </Grid>
             </Box>
           </Box>
         </Container>
-        </div>
-        
-        </>
-    );
-}
-export default AddAnswer
+      </div>
+    </>
+  );
+};
+export default AddAnswer;
--- AppPraktyki-main/src/pages/Details.tsx
+++ AppPraktyki-main/src/pages/Details.tsx
@@ -1,79 +1,68 @@
-import { useLocation, useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
-import Header from '../components/Header';
-import { List } from '@mui/material';
-import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
-const _details = [{
+const _details = [
+  {
     id: "0",
     name: "Równania kwadratowe",
     author: "Miłosz",
     rating: "7.6",
     text: "Zad1: a, zad2: b",
-},
-{
+  },
+  {
     id: "1",
     name: "Planimetria",
     author: "Bartosz",
     rating: "4.9",
     text: "Zad1: b, zad2: a",
-}]
+  },
+];
 
-const _comments=[
-    {
-        id_User:0,
-        user_name: "MiloszTT",
-        Text: "Dobra robota  d=====(￣▽￣*)b",
-        id_details:0
-    },
-    {
-        id_User:1,
-        user_name: "BartłomiejP",
-        Text: "odp a w z. 1 w gr A jest zle",
-        id_details:0
-    },
-    {
-        id_User:2,
-        user_name: "Skowronek_andrzej",
-        Text: "jakis kreatywny komentarz",
-        id_details:1
-    }
-]
+const _comments = [
+  {
+    id_User: 0,
+    user_name: "MiloszTT",
+    Text: "Dobra robota  d=====(￣▽￣*)b",
+    id_details: 0,
+  },
+  {
+    id_User: 1,
+    user_name: "BartłomiejP",
+    Text: "odp a w z. 1 w gr A jest zle",
+    id_details: 0,
+  },
+  {
+    id_User: 2,
+    user_name: "Skowronek_andrzej",
+    Text: "jakis kreatywny komentarz",
+    id_details: 1,
+  },
+];
 
-const commentblock=(id:number, aid:number)=>{
-    if(_comments[id].id_details==aid){
-            return  (
-                <div >
-                    <div>{_comments[id].user_name}</div>
-                    <div>{_comments[id].Text}</div>
-                    <br />
-                </div>
-            )
-        }
-}
-
+const commentblock = (id: number, aid: number) => {
+  if (_comments[id].id_details == aid) {
+    return (
+      <div>
+        <div>{_comments[id].user_name}</div>
+        <div>{_comments[id].Text}</div>
+        <br />
+      </div>
+    );
+  }
+};
 
 const Details = () => {
-    const location = useLocation();
-    const searchParams = new URLSearchParams(location.search);
-    const answerID=Number(searchParams.get('answerid'));
-    return (
-        <>
-            <Header accountName={"example"} subscriptionDaysLeft={"14"} />
-            <div>
-                <h2>{_details[answerID].name}</h2>
-            </div>
-            <div>
-                {_details[answerID].text}
-            </div>
-            <h4>{_details[answerID].rating}</h4>
-            <div>
-                {_comments.map((com, index) => (
-                    commentblock(index, answerID)
-            ))
-        }
-            </div>
-        </>
-    );
-}
-export default Details
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const answerID = Number(searchParams.get("answerid"));
+  return (
+    <>
+      <div>
+        <h2>{_details[answerID].name}</h2>
+      </div>
+      <div>{_details[answerID].text}</div>
+      <h4>{_details[answerID].rating}</h4>
+      <div>{_comments.map((com, index) => commentblock(index, answerID))}</div>
+    </>
+  );
+};
+export default Details;
