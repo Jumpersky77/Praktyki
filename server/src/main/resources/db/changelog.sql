b5238c02a6a97e1dce7532437b258abf4993a77f
Maszko <topmil77@gmail.com>
2024-04-22 11:27:27 +0200

    Changelog update

--- server/src/main/resources/db/changelog.sql
+++ server/src/main/resources/db/changelog.sql
@@ -17,7 +17,7 @@ CREATE TABLE answers (
 	grade float4 NULL,
 	id_teachers int4 NULL,
 	id_subject int4 NULL,
-	id int4 NOT NULL,
+	id SERIAL NOT NULL,
 	CONSTRAINT answers_pkey PRIMARY KEY (id)
 );
 
@@ -32,7 +32,7 @@ CREATE TABLE "comments" (
 	id_student int4 NULL,
 	id_answer int4 NULL,
 	comment_text text NULL,
-	id int4 NOT NULL,
+	id SERIAL NOT NULL,
 	CONSTRAINT comments_pk PRIMARY KEY (id)
 );
 
@@ -47,7 +47,7 @@ CREATE TABLE grades (
 	id_student int4 NULL,
 	id_answer int4 NULL,
 	grade int4 NULL,
-	id int4 NOT NULL,
+	id SERIAL NOT NULL,
 	CONSTRAINT grades_pkey PRIMARY KEY (id)
 );
 
@@ -61,7 +61,7 @@ CREATE TABLE grades (
 CREATE TABLE image (
 	id_answer int4 NULL,
 	raster oid NULL,
-	id int4 NOT NULL,
+	id SERIAL NOT NULL,
 	CONSTRAINT image_pkey PRIMARY KEY (id)
 );
 
@@ -76,7 +76,7 @@ CREATE TABLE student (
 	slogin varchar(50) NULL,
 	spassword varchar(50) NULL,
 	sadmins int4 DEFAULT 0 NULL,
-	id int4 NOT NULL,
+	id SERIAL NOT NULL,
 	CONSTRAINT student_pkey PRIMARY KEY (id)
 );
 
@@ -120,11 +120,65 @@ CREATE TABLE teachers_subject (
 	CONSTRAINT teachers_subject_pk PRIMARY KEY (id_teachers, id_subject)
 );
 
-ALTER TABLE teachers_subject ADD CONSTRAINT teachers_subject_subject_fk FOREIGN KEY (id_subject) REFERENCES subject(id) ON DELETE CASCADE;
-ALTER TABLE teachers_subject ADD CONSTRAINT teachers_subject_teachers_fk FOREIGN KEY (id_teachers) REFERENCES teachers(id);
-ALTER TABLE answers ADD CONSTRAINT answers_teachers_fk FOREIGN KEY (id) REFERENCES teachers(id);
-ALTER TABLE "comments" ADD CONSTRAINT comments_answers_fk FOREIGN KEY (id_answer) REFERENCES answers(id) ON DELETE CASCADE;
-ALTER TABLE "comments" ADD CONSTRAINT comments_student_fk FOREIGN KEY (id_student) REFERENCES student(id);
-ALTER TABLE grades ADD CONSTRAINT grades_answers_fk FOREIGN KEY (id_answer) REFERENCES answers(id) ON DELETE CASCADE;
-ALTER TABLE grades ADD CONSTRAINT grades_student_fk FOREIGN KEY (id_student) REFERENCES student(id);
-ALTER TABLE image ADD CONSTRAINT image_answers_fk FOREIGN KEY (id_answer) REFERENCES answers(id) ON DELETE CASCADE;
+--ALTER TABLE teachers_subject ADD CONSTRAINT teachers_subject_subject_fk FOREIGN KEY (id_subject) REFERENCES subject(id) ON DELETE CASCADE;
+--ALTER TABLE teachers_subject ADD CONSTRAINT teachers_subject_teachers_fk FOREIGN KEY (id_teachers) REFERENCES teachers(id);
+--ALTER TABLE answers ADD CONSTRAINT answers_teachers_fk FOREIGN KEY (id) REFERENCES teachers(id);
+--ALTER TABLE "comments" ADD CONSTRAINT comments_answers_fk FOREIGN KEY (id_answer) REFERENCES answers(id) ON DELETE CASCADE;
+--ALTER TABLE "comments" ADD CONSTRAINT comments_student_fk FOREIGN KEY (id_student) REFERENCES student(id);
+--ALTER TABLE grades ADD CONSTRAINT grades_answers_fk FOREIGN KEY (id_answer) REFERENCES answers(id) ON DELETE CASCADE;
+--ALTER TABLE grades ADD CONSTRAINT grades_student_fk FOREIGN KEY (id_student) REFERENCES student(id);
+--ALTER TABLE image ADD CONSTRAINT image_answers_fk FOREIGN KEY (id_answer) REFERENCES answers(id) ON DELETE CASCADE;
+
+insert into teachers values (1,'Ewa','Znamirowska');
+insert into teachers values (2,'Agnieszka','Korzańska');
+insert into teachers values (3,'Iryna','Mashyna');
+insert into teachers values (4,'Małgorzata','Bulwit');
+insert into teachers values (5,'Katarzyna','Kuś-Chatys');
+
+INSERT INTO public.subject
+(namesubject, id)
+VALUES('Matematyka', 1);
+INSERT INTO public.subject
+(namesubject, id)
+VALUES('Język Angielski', 2);
+INSERT INTO public.subject
+(namesubject, id)
+VALUES('Język Niemiecki', 3);
+
+INSERT INTO public.grades
+(id_student, id_answer, grade)
+VALUES(2, 1, 10);
+
+INSERT INTO public.teachers_subject
+(id_teachers, id_subject)
+VALUES(1, 1);
+INSERT INTO public.teachers_subject
+(id_teachers, id_subject)
+VALUES(2, 2);
+INSERT INTO public.teachers_subject
+(id_teachers, id_subject)
+VALUES(3, 3);
+INSERT INTO public.teachers_subject
+(id_teachers, id_subject)
+VALUES(4, 2);
+INSERT INTO public.teachers_subject
+(id_teachers, id_subject)
+VALUES(5, 3);
+
+INSERT INTO public."comments"
+(id_student, id_answer, comment_text)
+VALUES(2, 1, 'Dobra robota!');
+
+INSERT INTO public.answers
+(student_id, add_data, text_p, text_a, grade, id_teachers, id_subject)
+VALUES(1, '2024-04-13', 'luctus cum sociis natoque penatibus et magnis dis parturient montes nascetur ridiculus mus vivamus vestibulum sagittis sapien cum sociis natoque', 'turpis enim blandit mi in porttitor pede justo eu massa donec dapibus duis at velit eu est congue elementum in', 64.3, 1, 1);
+
+INSERT INTO public.student
+(slogin, spassword, sadmins)
+VALUES('Antoni', 'kI3$9}.y*&', 0);
+INSERT INTO public.student
+(slogin, spassword, sadmins)
+VALUES('Bartlomiej', '123', 1);
+INSERT INTO public.student
+(slogin, spassword, sadmins)
+VALUES('Piotr', 'Piotr', 5);
