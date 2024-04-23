d1648373c3516ca63645901c15c255e8dfa8ae41
Bajtek <bartox401@gmail.com>
2024-04-23 15:30:49 +0200

    Answers update

--- server/src/main/java/com/softserve/ezn4/praktyki/answers/AnswerDAO.java
+++ server/src/main/java/com/softserve/ezn4/praktyki/answers/AnswerDAO.java
@@ -1,9 +1,11 @@
 package com.softserve.ezn4.praktyki.answers;
 
+import com.softserve.ezn4.praktyki.teachers.TeacherMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public class AnswerDAO {
@@ -20,5 +22,15 @@ public class AnswerDAO {
                 VALUES (?,?,?,?,?,?,?,?)
                  """;
         return jdbcTemplate.update(sql, studentID, teacersID, subjectID, textA, textP, grade, new java.sql.Date(date.getTime()), type.name());
+    }
+
+    public List<AnswerDTO> findAnswersByTeacher(Long teacherID){
+        var sql = """
+            SELECT answers.id, student.slogin, grade, text_p, answer_type
+            FROM answers
+            INNER JOIN student ON student.id = answers.student_id
+            WHERE id_teachers = ?
+             """;
+        return jdbcTemplate.query(sql, new AnswerMapper(), teacherID);
     }
 }
